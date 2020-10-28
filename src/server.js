8534410125df1ec0e42342a7c9b9f7c207855496
Maria Sokolova <sb_mv@inbox.ru>
2020-10-28 13:38:09 +0100

    feat: add dropDateBase

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
+const { User } = require('./resources/users/user.model');
 
 const { PORT, MONGO_CONNECTION_STRING } = require('./common/config');
 const app = require('./app');
 const logger = require('./common/winston');
+
+const users = [
+  new User({ name: 'user1', login: 'admin', password: 'admin' }),
+  new User({ name: 'user2', login: 'login2', password: 'Secret' })
+];
 
 mongoose.connect(MONGO_CONNECTION_STRING, {
   useNewUrlParser: true,
@@ -12,12 +18,12 @@ mongoose.connect(MONGO_CONNECTION_STRING, {
 
 const db = mongoose.connection;
 
-db.on('error', () => logger.error('MongoDB connection error:')).once(
-  'open',
-  () => {
-    logger.info('Successfully connect to DB');
-    app.listen(PORT, () =>
-      logger.info(`App is running on http://localhost:${PORT}`)
-    );
-  }
-);
+db.on('error', () => logger.error('MongoDB connection error:'));
+db.once('open', () => {
+  logger.info('Successfully connect to DB');
+  db.dropDatabase();
+  users.forEach(user => user.save());
+  app.listen(PORT, () =>
+    logger.info(`App is running on http://localhost:${PORT}`)
+  );
+});
